e0de33bc4ddc7068615190180728fa16324d0d2b
Merveux <mervinkwizera@gmail.com>
2026-07-16 15:00:48 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+function buyTicket(){
+
+alert(
+"Ticket added successfully!"
+);
+
+}
+
+
+
+function registerFan(){
+
+let name =
+document.getElementById("fanName").value;
+
+
+document.getElementById("message")
+.innerHTML =
+"Welcome "+name+"! Fan account created.";
+
+}
+
+
+
+
+function addCart(){
+
+alert(
+"Product added to cart!"
+);
+
+}
