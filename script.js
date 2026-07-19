5835fa7b02ca970dce2d88f834189fd1b119f521
Merveux <mervinkwizera@gmail.com>
2026-07-19 15:54:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,266 @@
-function buyTicket(){
 
-alert(
-"Ticket added successfully!"
-);
+
+// Welcome message
+
+function welcome(){
+
+alert("Welcome to Arsenal Family 🔴⚪ COYG!");
 
 }
 
 
 
-function registerFan(){
+// Store purchase
+
+function buy(){
+
+alert("Thank you for supporting Arsenal!");
+
+}
+
+
+
+// Register fan
+
+function register(){
+
+
+let name=document.getElementById("name").value;
+
+
+let fans =
+JSON.parse(localStorage.getItem("fans")) || [];
+
+
+
+fans.push(name);
+
+
+
+localStorage.setItem(
+"fans",
+JSON.stringify(fans)
+);
+
+
+
+alert("Registration successful!");
+
+window.location="login.html";
+
+
+}
+
+
+
+
+// Login system
+
+
+function login(){
+
+
+
+let username =
+document.getElementById("username").value;
+
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+
+if(username=="admin" && password=="1234"){
+
+
+window.location="admin.html";
+
+
+}
+
+
+else{
+
+
+alert("Invalid login details");
+
+
+}
+
+
+}
+
+
+
+
+// Add player from admin
+
+
+function addPlayer(){
+
+
 
 let name =
-document.getElementById("fanName").value;
+document.getElementById("playerName").value;
 
 
-document.getElementById("message")
-.innerHTML =
-"Welcome "+name+"! Fan account created.";
+
+let position =
+document.getElementById("playerPosition").value;
+
+
+
+
+let players =
+JSON.parse(localStorage.getItem("players")) || [];
+
+
+
+
+players.push({
+
+name:name,
+
+position:position
+
+
+});
+
+
+
+
+localStorage.setItem(
+"players",
+JSON.stringify(players)
+
+);
+
+
+
+alert("Player added successfully!");
+
+loadPlayers();
+
 
 }
 
 
 
 
-function addCart(){
+// Display players in admin page
 
-alert(
-"Product added to cart!"
-);
+
+function loadPlayers(){
+
+
+
+let area =
+document.getElementById("players");
+
+
+
+if(area){
+
+
+let players =
+JSON.parse(localStorage.getItem("players")) || [];
+
+
+
+area.innerHTML="";
+
+
+
+players.forEach(function(player){
+
+
+area.innerHTML +=
+
+`
+
+<div class="box">
+
+<h3>${player.name}</h3>
+
+<p>${player.position}</p>
+
+
+</div>
+
+`;
+
+
+
+});
+
+
+}
+
+
+}
+
+
+
+
+// Display fans
+
+
+function loadFans(){
+
+
+
+let area =
+document.getElementById("fans");
+
+
+
+if(area){
+
+
+
+let fans =
+JSON.parse(localStorage.getItem("fans")) || [];
+
+
+
+area.innerHTML="";
+
+
+
+fans.forEach(function(fan){
+
+
+area.innerHTML +=
+
+`
+
+<div class="box">
+
+<h3>${fan}</h3>
+
+</div>
+
+`;
+
+
+});
+
+
+}
+
+
+
+}
+
+
+
+
+window.onload=function(){
+
+loadPlayers();
+
+loadFans();
 
 }
